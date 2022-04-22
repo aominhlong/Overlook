1e368b3383d61ac20809ef5522192a5130a277f7
Nicholas Ao <aominhlong@yahoo.com>
2022-04-21 21:36:26 -0600

    Refactor main page and user login page

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,25 @@
 // This is the JavaScript entry file - your code begins here
-const topHalf = document.querySelector('top-half')
-const currentBookings = document.querySelector('current-bookings')
-const userArea = document.querySelector('user-info')
-
-
-
-const checkDateBtn = document.querySelector(".check-date-btn")
+const topHalf = document.querySelector('.top-half')
+const bottomHalf = document.querySelector('.bottom-half')
+const currentBookings = document.querySelector('.current-bookings')
+const userArea = document.querySelector('.user-info')
 const date = document.querySelector('input[type="date"]')
+const checkDateBtn = document.querySelector(".check-date-btn")
 
+// **login page selectors**
+const loginBtn = document.querySelector('.login-btn')
+const loginPage = document.querySelector('.login-page')
+const dashboardTitle = document.querySelector('.title')
+
+
+
+// *****On Window load*****
+window.addEventListener('load', () => {
+    hideAll([topHalf, bottomHalf])
+})
+
+
+// ***** LOGIN PAGE *****
 checkDateBtn.addEventListener('click', () => {
     checkDate()
 })
@@ -15,22 +27,40 @@ checkDateBtn.addEventListener('click', () => {
 const checkDate = () => {
     console.log(date.value)
 }
-// *****On load*****
-window.addEventListener('load', () => {
- 
+
+loginBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    hideAll([loginPage])
+    showAll([topHalf, bottomHalf])
 })
 
 
 // ***** DASHBOARD *****
-const createMainPage = () => {
-    hideAll([topHalf, currentBookings, userArea])
-}
+
 
 const hideAll = (array) => {
   array.forEach((element) => {
     element.classList.add("hidden")
   })
 }
+
+const showAll = (array) => {
+    array.forEach((element) => {
+      element.classList.remove("hidden")
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 // Do not delete or rename this file ********
 
 
