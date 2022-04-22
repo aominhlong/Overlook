80609463fa06cf156ea34dc810781fa1ef37ebe5
Nicholas Ao <aominhlong@yahoo.com>
2022-04-21 20:37:36 -0600

    Add login page

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,7 @@
 // This is the JavaScript entry file - your code begins here
-
+const topHalf = document.querySelector('top-half')
+const currentBookings = document.querySelector('current-bookings')
+const userArea = document.querySelector('user-info')
 
 
 
@@ -13,8 +15,22 @@ checkDateBtn.addEventListener('click', () => {
 const checkDate = () => {
     console.log(date.value)
 }
+// *****On load*****
+window.addEventListener('load', () => {
+ 
+})
 
 
+// ***** DASHBOARD *****
+const createMainPage = () => {
+    hideAll([topHalf, currentBookings, userArea])
+}
+
+const hideAll = (array) => {
+  array.forEach((element) => {
+    element.classList.add("hidden")
+  })
+}
 // Do not delete or rename this file ********
 
 
